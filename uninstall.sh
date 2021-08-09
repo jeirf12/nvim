7fc4da2fd6a0c0b76db737810ad18284d7cb3d09
jeirf12 <jeirf12@github.com>
2021-08-09 11:07:41 -0500

    verifies the existence of the files

--- uninstall.sh
+++ uninstall.sh
@@ -16,8 +16,36 @@ function ctrl_c(){
 	exit 0
 }
 
-function uninstallSettings(){
+function existSettings(){
 	tput civis; clear
+	echo -e "\n${purpleColour}[*]${endColour}${blueColour} comprobando la existencia de configuraciones en neovim${endColour}"
+	sleep 2
+	flag=0
+	test -d ~/.config/nvim/
+	if [ "$(echo $?)" == "0" ]; then
+		flag=$((flag + 1))
+	fi
+	test -f ~/.config/.vimrc
+	if [ $(echo $?) == "0" ]; then
+		flag=$((flag + 1))
+	fi
+	test -d ~/.vim/plugged/
+	if [ $(echo $?) == "0" ]; then
+		flag=$((flag + 1))
+	fi
+	test -d ~/.config/coc/
+	if [ $(echo $?) == "0" ]; then
+		flag=$((flag + 1))
+	fi
+	if [ "$flag" -eq "0" ]; then
+		echo -e "\n${yellowColour} No se puede desinstalar, porque no existe referencias del programa${endColour}"
+		ctrl_c
+	fi;
+	echo -e "\n${purpleColour}[*]${endColour}${blueColour} comprobación finalizada...${endColour}"
+	sleep 1
+}
+
+function uninstallSettings(){
 	echo -e "\n${purpleColour}[*]${endColour}${blueColour} Desinstalando las configuraciones de neovim${endColour}"
 	rm -rf ~/.config/nvim/ ~/.config/.vimrc ~/.config/coc/ ~/.vim/plugged/ > /dev/null 2>&1
 
@@ -29,5 +57,6 @@ function uninstallSettings(){
 }
 
 # Inicia el script de desinstalación
+existSettings
 uninstallSettings
 # Termina el script de desinstalación
